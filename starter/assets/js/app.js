42914066b4224a36b233e7e2384e27f7d2c0338e
ErrayAbdellah <mahdiabdllah121@gmail.com>
2022-10-20 16:59:38 +0100

    edit 20/10/2022

--- starter/assets/js/app.js
+++ starter/assets/js/app.js
@@ -4,8 +4,8 @@
  */
  let title = document.getElementById("titleIn");
  let type = document.querySelectorAll("input[name='Type']");
- let priority = document.getElementById("select1");
- let statuss = document.getElementById("select2");
+ let statuss = document.getElementById("select1");
+ let priority = document.getElementById("select2");
  let date = document.getElementById("date");
  let description = document.getElementById("exampleFormControlTextarea1");
  let submit = document.getElementById("submit");
@@ -22,27 +22,15 @@
  progress.innerHTML = "";
  done.innerHTML = "";
 
-    reloadTasks();
+var btn = document.querySelector(".btnBtn");
 
 
+ let newTasks = [] ;
+ newTasks = tasks ; 
+ console.log(newTasks) ;
+ 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+reloadTasks();
  
 //  submit.onclick = function() {
 //      let create ={
@@ -57,14 +45,34 @@
 //      console.log(create);
 //  }
 
+
+
+// function reloadTasks() {
+//     // Remove tasks elements
+    
+//     // Set Task count
+//     for(var i=0;i<tasks.length;i++)
+//     {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+//         reloadtask(i);
+//     }
+    
+// }
+
+
+
+
 function createTask() {
     
-  
+//   initTaskForm() ;
     
     // Afficher le boutton save
     // if(title.value =='' && select1.selectedIndex == 0 &&  select2.selectedIndex == 0 && date.value=='' && description.value=='' )
     // submit.visible
 
+    todo.innerHTML = "";
+    progress.innerHTML = "";
+    done.innerHTML = "";
+   
 
     // Ouvrir modal form
 
@@ -81,21 +89,23 @@ function createTask() {
             {
                  title:title.value,
                  type:radioChecked,
-                 priority:priority.value,
                  status:statuss.value,
+                 priority:priority.value,
                  date:date.value,
                  description:description.value,
              }
             //  console.log(create);                                                                                                                                                                                                                                                                                                                                                                                                      
-             tasks.push(create);  
-             console.log(tasks);
+             newTasks.push(create);  
+             console.log(newTasks) ;
+            //  console.log(tasks);
+             reloadTasks() ;
 
 // initialiser task form
              title.value = type.value   = date.value = description.value ='';
              select1.selectedIndex = 0 ;
-             select2.selectedIndex = 0 ;
+             select2.selectedIndex = 0 ;            
              
-    
+            //  reloadTasks();
 }
 
 function saveTask() {
@@ -111,6 +121,15 @@ function saveTask() {
 
 function editTask(index) {
     // Initialisez task form
+
+
+    title.value 
+    type.value   = 
+    date.value = 
+    description.value ='';
+    select1.selectedIndex = 0 ;
+    select2.selectedIndex = 0 ;
+
 
     // Affichez updates
 
@@ -148,45 +167,85 @@ function deleteTask() {
 
 function initTaskForm() {
     // Clear task form from data
+    // let tasksToDelte = document.querySelectorAll(".task");
 
+    // for(t in tasksToDelte)
+    // {
+    //     console.log("element to delete"+t);
+    //     // t.remove();
+    //     console.log("hi!!") ;
+    // }
     // Hide all action buttons
 }
+
 function reloadTasks() {
     // Remove tasks elements
     
-    
-
     // Set Task count
-    for(var i=0;i<tasks.length;i++)
+    for(let i=0;i<newTasks.length;i++)
     {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
         reloadtask(i);
     }
     
 }
 
-function reloadtask(i)
-{
-   
-    
+
+function reloadtask(i) 
+{    
+    // console.log(tasks.length) ;
+    // console.log(tasks[i]) ;
+    // console.log("here "+tasks.length) ;
+
     let cmpTodo = 0;
     let cmpProgresse = 0;
     let cmpDon = 0; 
-      
-        if(tasks[i]["status"]=="To Do") 
-        { 
-            todo.innerHTML += '<button class="border-1 border-secondary d-flex" style="background-color: #CAEBF2;"><div class="col-1"><i class="bi bi-question-circle text-success fa-2x"></i>				</div>								<div class="text-start col-11">									<div class="fw-bold">'+tasks[i]["title"]+'</div><div class=""><div class="text-secondary-300">#1 created in '+tasks[i]["date"]+'</div><div class="" title="'+tasks[i]["description"]+'">'+tasks[i]["description"].slice(0,55)+'...</div></div><div class=""><span class="btn btn-info rounded-pill">High</span>	<span class="btn btn-gray-500 rounded-pill">Feature</span></div></div></button>';
-            cmpTodo++;
-        }else if(tasks[i]["status"]=="In Progress")
-        {
-            progress.innerHTML += '<button class="border-1 border-secondary d-flex" style="background-color: #CAEBF2;"><div class="col-1"><i class="bi bi-question-circle text-success fa-2x"></i>				</div>								<div class="text-start col-11">									<div class="fw-bold">'+tasks[i]["title"]+'</div><div class=""><div class="text-secondary-300">#1 created in '+tasks[i]["date"]+'</div><div class="" title="'+tasks[i]["description"]+'">'+tasks[i]["description"].slice(0,55)+'...</div></div><div class=""><span class="btn btn-info rounded-pill">High</span>	<span class="btn btn-gray-500 rounded-pill">Feature</span></div></div></button>';
-            cmpProgresse++;
-        }else if(tasks[i]["status"]=="Done")
-        {
-            done.innerHTML += '<button class="border-1 border-secondary d-flex" style="background-color: #CAEBF2;"><div class="col-1"><i class="bi bi-question-circle text-success fa-2x"></i>				</div>								<div class="text-start col-11">									<div class="fw-bold">'+tasks[i]["title"]+'</div><div class=""><div class="text-secondary-300">#1 created in '+tasks[i]["date"]+'</div><div class="" title="'+tasks[i]["description"]+'">'+tasks[i]["description"].slice(0,55)+'...</div></div><div class=""><span class="btn btn-info rounded-pill">High</span>	<span class="btn btn-gray-500 rounded-pill">Feature</span></div></div></button>';
-            cmpDon++;
-        }
-      
+    if(newTasks[i]["status"]=="To Do") 
+    { 
+    todo.innerHTML += `<button onclick="reset(this)"class="btnBtn border-1 border-secondary d-flex" >
+                            <div class="col-1">
+                                <i class="bi bi-question-circle text-success fa-2x"></i>
+                            </div>								
+                            <div class="text-start col-11">									
+                                <div class="fw-bold">
+                                    `+newTasks[i]["title"]+`
+                                </div>
+                                <div class="">
+                                    <div class="text-secondary-300">
+                                        #1 created in `+newTasks[i]["date"]+`
+                                    </div>
+                                    <div class="" title="'+newTasks[i]["description"]+'">
+                                        `+newTasks[i]["description"].slice(0,55)+`...
+                                    </div>
+                                </div>
+                                <div class="">
+                                    <span class="btn btn-info rounded-pill">High</span>	
+                                    <span class="btn btn-gray-500 rounded-pill">Feature</span>
+                                </div>
+                            </div>
+                        </button>`;
+        cmpTodo++;
+    }else if(newTasks[i]["status"]=="In Progress")
+    {
+        progress.innerHTML += '<button  onclick="reset(this)" class="btnBtn border-1 border-secondary d-flex" ><div class="col-1"><i class="bi bi-question-circle text-success fa-2x"></i>				</div>								<div class="text-start col-11">									<div class="fw-bold">'+newTasks[i]["title"]+'</div><div class=""><div class="text-secondary-300">#1 created in '+newTasks[i]["date"]+'</div><div class="" title="'+newTasks[i]["description"]+'">'+newTasks[i]["description"].slice(0,55)+'...</div></div><div class=""><span class="btn btn-info rounded-pill">High</span>	<span class="btn btn-gray-500 rounded-pill">Feature</span></div></div></button>';
+        cmpProgresse++;
+    }else if(newTasks[i]["status"]=="Done")
+    {
+        done.innerHTML += '<button onclick="reset(this)" class="btnBtn border-1 border-secondary d-flex" ><div class="col-1"><i class="bi bi-question-circle text-success fa-2x"></i>				</div>								<div class="text-start col-11">									<div class="fw-bold">'+newTasks[i]["title"]+'</div><div class=""><div class="text-secondary-300">#1 created in '+newTasks[i]["date"]+'</div><div class="" title="'+newTasks[i]["description"]+'">'+newTasks[i]["description"].slice(0,55)+'...</div></div><div class=""><span class="btn btn-info rounded-pill">High</span>	<span class="btn btn-gray-500 rounded-pill">Feature</span></div></div></button>';
+        cmpDon++;
+    }
 }
-
     
 
+function reset(item)
+{
+    
+    $('#modal-task').modal('show');
+    // var titler = selector.querySelector('#title').value = selector.value  // <input id="title" value="ghj"
+    // title.value = ;
+    // type.value   = 
+    // date.value = 
+    // description.value ='';
+    // select1.selectedIndex = 0 ;
+    // select2.selectedIndex = 0 ; 
+    console.log(item);
+}
